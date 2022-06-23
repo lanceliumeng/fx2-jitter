ecc1fdb61a9fb7ba95a4c02a80df67ba7d58314b
Lance Liu <lanceliumeng89@gmail.com>
2022-06-23 14:50:31 +1000

    change PORT back to rails backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,22 +1,21 @@
 // information from the backend, URL
 
-import axios from 'axios'
+import axios from "axios";
 
 const jitterAPI = axios.create({
-    baseURL: 'http://localhost:4000'
-})
+  baseURL: "http://localhost:4000",
+});
 
-jitterAPI.interceptors.request.use(req => {
-    // send the token in the request
-    const token = sessionStorage.getItem("token")
-    console.log(token)
-    // Authorization -> Bearer token -> paste the token
-    if (token) {
-        req.headers["Authorization"] = `Bearer ${token}`
-    }
-    
+jitterAPI.interceptors.request.use((req) => {
+  // send the token in the request
+  const token = sessionStorage.getItem("token");
+  console.log(token);
+  // Authorization -> Bearer token -> paste the token
+  if (token) {
+    req.headers["Authorization"] = `Bearer ${token}`;
+  }
 
-    return req
-})
+  return req;
+});
 
-export default jitterAPI
+export default jitterAPI;
